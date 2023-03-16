75047460164b13cac9e1e73f11b71a48388e9701
Thomas Kilgour <thomas@kilgour.co>
2023-03-16 11:11:52 -0400

    update cv

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
           <NavItem href="/projects">Projects</NavItem>
           <NavItem href="/photography">Photography</NavItem>
           <NavItem href="/about">About</NavItem>
-          <NavItem href="/Thomas%20Kilgour%20resume.pdf" download>
+          <NavItem href="/Thomas%20Kilgour%20CV.pdf" download>
             CV
           </NavItem>
         </ul>
