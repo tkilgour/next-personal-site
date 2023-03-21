0bed4fb51177d58e3a35ae5cd27088747e1d7780
Thomas Kilgour <thomas@kilgour.co>
2023-03-21 16:15:44 -0400

    add post preview in development

--- helpers/getPosts.js
+++ helpers/getPosts.js
@@ -4,19 +4,22 @@ import matter from "gray-matter";
 
 const getPosts = () => {
   const files = fs.readdirSync(path.join("posts"));
-  const allPostsData = files.map((fileName) => {
-    const slug = fileName.replace(".mdx", "");
-    const fileContents = fs.readFileSync(
-      path.join(`posts/${slug}.mdx`),
-      "utf8"
+  const allPostsData = files
+    .map((fileName) => {
+      const slug = fileName.replace(".mdx", "");
+      const fileContents = fs.readFileSync(
+        path.join(`posts/${slug}.mdx`),
+        "utf8"
+      );
+      const { data } = matter(fileContents);
+      return {
+        slug,
+        data,
+      };
+    })
+    .filter(
+      (post) => process.env.NODE_ENV == "development" || post.data.published
     );
-    const { data } = matter(fileContents);
-    return {
-      slug,
-      data,
-    };
-  }).filter(post => post.data.published);
-
   return allPostsData;
 };
 
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,14 @@ export default function Home({ posts }) {
 
       <ul>
         {posts.map((post) => (
-          <li key={post.slug} className="mb-32">
+          <li
+            key={post.slug}
+            className={`mb-20 ${
+              process.env.NODE_ENV === "development" &&
+              post.data.published === false &&
+              "border border-red-400 border-dashed"
+            }`}
+          >
             <h2 className="mb-2">
               <Link
                 className="text-2xl outline-none underline-offset-8 hover-color-secondary hover:underline focus:underline"
@@ -21,7 +28,9 @@ export default function Home({ posts }) {
               </Link>
             </h2>
             <div className="text-sm mb-8">{post.data.date}</div>
-            <p className="text-gray-500 dark:text-gray-400">{post.data.description}</p>
+            <p className="text-gray-500 dark:text-gray-400">
+              {post.data.description}
+            </p>
           </li>
         ))}
       </ul>
@@ -30,11 +39,11 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = () => {
-  const posts = getPosts()
+  const posts = getPosts();
 
   return {
     props: {
-      posts
-    }
-  }
-}
+      posts,
+    },
+  };
+};
