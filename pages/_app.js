2c5ffe53fffde8aef2de6f6f417b9b6800020b86
Thomas Kilgour <thomas@kilgour.co>
2023-04-05 15:49:10 -0400

    working on OneSignal integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,36 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import { useEffect } from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "e8472fa1-f22c-4943-8147-ce9ba1407c2c",
+        notifyButton: {
+          enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    }
+  }, []);
+  
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.svg" />
       </Head>
+      <Script
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        async=""
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
